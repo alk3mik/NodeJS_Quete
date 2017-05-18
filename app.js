c80fa130ccb32fdd566daff5be4c0d261655124c
alk3mik <mozz76@gmail.com>
2017-05-19 00:51:43 +0200

    Looks like I did it

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 'use strict';
 
-const currYear = new Date();
-// console.log(currYear);
-const currYearMS = currYear.getTime();
-// console.log(currYear, currYearMS);
+// The Date() function creates the date in the following format 2017-05-18T22:46:02.002Z .
+// The method getTime() converts it into the milliseconds elapsed from the year 1970.
+var currYearMS = new Date().getTime();
+
+// This is the conversion factor from year to milliseconds.
+const y2ms = 31556952000;
+
+// Therefore we define also the offset:
+const offset = 1970;
+
 var birthYear;
-const y2ms = 22896000000;
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8'); 
 console.log('How old are you?');
    
 process.stdin.on('data', function(age) {
-	birthYear = (currYearMS - age * y2ms) / y2ms;
-    console.log('You were born in ' + birthYear);
-    process.exit();
+
+	if (isNaN(age)) {
+		
+		console.log('Age value not valid. Please enter a numeric value.');
+	
+	} else if (age > 99) {
+	
+		console.log('Age value exceeds the upper limit. Please enter a number less than 99 .')
+	
+	} else {
+	
+		birthYear = Math.floor((currYearMS - age * y2ms) / y2ms) + offset;
+    	console.log('You were born in ' + birthYear);
+    	process.exit();
+    
+    }
+
 });
